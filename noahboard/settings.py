cc5110e6fcd3f73ecbfd69a9cc7e392e08dc573a
jinguanz <berkzhou@gmail.com>
2013-08-19 00:51:04 -0700

    add commnets

--- noahboard/settings.py
+++ noahboard/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
+# Set the time zone
 TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:

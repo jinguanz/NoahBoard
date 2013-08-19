0c1e0269e49446093843dc89269bb7606ce424ae
jinguanz <berkzhou@gmail.com>
2013-08-19 00:55:55 -0700

    Add new comments

--- noahboard/settings.py
+++ noahboard/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
 # Set the time zone
+# Add comments
 TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
